da1f4cb3893691b9587422c0293fa7933db31df6
bayeng <88835193+bayeng@users.noreply.github.com>
2024-03-18 03:07:39 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      letterSpacing: {
+        ngangkang: '.5em'
+      }
+    },
+  },
+  plugins: [],
+}
+
